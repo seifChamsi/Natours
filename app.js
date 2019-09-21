1d180e4cd42d3945b01f53105f6d93a9bdc02c2d
Saif chamsi <saif.chamsi99@gmail.com>
2019-09-21 12:40:12 +0100

    Setting new features
    adding the Create&Read tours feature

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        name: "saif",
-        University: "ISET SOUSSE"
-    });
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200)
+        .json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        })
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({
+        id: newId
+    }, req.body);
+
+    tours.push(newTour);
+    //adding a new tour
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(200)
+            .json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            })
+    })
+
 })
 
+
 const port = 3000;
-app.listen(port, () => {
-    console.log(`[+]App is running on PORT: ${port}`);
+app.listen(port, '127.0.0.1', () => {
+    console.log("[*] App is listening on port : 3000");
 })
